ed2a62aadd9f35264bd783f639021b3fb9d32265
benjam-rich <70960034+benjam-rich@users.noreply.github.com>
2021-01-29 16:00:05 -0700

    Add project files.

---
+++ CinemaProj/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using CinemaProj.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaProj.Controllers
+    /*This page controls what vies will display and what information is passed to each view. Very useful */
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult newMovies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult newMovies(ApplicationResponse appResponse)
+        {
+            TempStorage.AddApplication(appResponse);
+            return View("Movies", appResponse);
+        }
+
+        //This Action will display all the responses in temporary storage to the Movie List page as long as they're not Indendence Day
+        public IActionResult List()
+        {
+            return View(TempStorage.Applications.Where(x => x.Title != "Independence Day"));
+        }
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CinemaProj/Models/ApplicationResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaProj.Models
+{
+/*Here is the aplication response page for validation and getting and setting the values from the form for the model */
+    public class ApplicationResponse
+    {
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Year { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public string Rating { get; set; }
+        public string Edited { get; set; }
+        public string LentTo { get; set; }
+        [MaxLength(25)]
+        public string Notes { get; set; }
+    }
+}
